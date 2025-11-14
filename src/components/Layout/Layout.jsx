37c3b3a4cd69beef219011a21cf7a85721a7b004
sujeetyadav6918-byte <sujeetyadav6918@gmail.com>
2025-11-14 11:01:29 +0530

    Fix: updated Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import { Outlet } from 'react-router-dom'
-import FooterSection from '../footersection/Footersection'
+// import FooterSection from '../footersection/Footersection'
+import FooterSection from '../footersection/Footersection';
+
 
 
 
